3d05247fe28463fb4b9e4ceda9ed6947614378b9
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-02-18 20:21:16 -0300

    Add Home Page styles

--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -1,14 +1,21 @@
-import styled, {css} from "styled-components";
+import styled from "styled-components";
 import bgImage from "../../img/happy.png";
 
 export const HomePage = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+position: relative;
 background-color: #8C52E5;
 background-image: url(${bgImage});
 background-repeat: no-repeat;
 background-position: bottom;
+
 height: 100vh;
     img{
-        margin: 40px;
+        position: absolute;
+        top: 40px;
+        left: 40px;
     }
     .middle-div{
         display: flex;
@@ -20,19 +27,25 @@ height: 100vh;
             color: white;
             font-size: 20px;
             h2{
+                font-size: 29px;
+                font-weight: normal;
                 margin-bottom: 25px;
             }
             button{
-                cursor: pointer;
-                border: none;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                
+                border: 0;
                 border-radius: 100px;
                 padding: 15px 30px;
                 width: 250px;
                 text-align: left;
                 transition: 0.5s;
                 color: #8C52E5;
-                font-size: 20px;
-                font-weight: 300;
+                font-size: 18px;
+                font-weight: 500;
+                background: #FFF;
                 &:hover{
                     background: #68DE5A;
                 }
@@ -41,46 +54,48 @@ height: 100vh;
         .signup-div{
             background: white;
             border-radius: 15px;
-            padding: 20px;
+            padding: 40px;
             flex: 0;
-            
-            div{
-                margin: 20px;
+                   
+            h4{
+                font-size: 25px;
+                font-weight: 500;
             }
+
             form{
                 input{
-                    display:block;
-                    border: none;
+                    border: 0;
                     font-size: 16px;
                     padding: 8px;
-                    font-weight: 300;
+                    font-weight: 500;
                     text-align:left;
-                    width: 90%;
-                    margin: 25px;
-                    border-bottom: 2px solid #666;
+                    width: 100%;
+                    color: #878686;
+                    margin: 32px 0 0;
+                    border-bottom: 2px solid #878686;
                 }
             }
             
-            h4{
-                font-size: 25px;
-                margin-bottom:20px;
-            }
-
             button{
-                cursor: pointer;
-                border: none;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+
+                border: 0;
                 border-radius: 100px;
                 padding: 15px 30px;
                 width: 325px;
                 text-align: left;
                 transition: 0.5s;
-                color: #8C52E5;
-                font-size: 20px;
-                font-weight: 300;
-                border: 3px solid #8C52E5;
-                margin: 20px 0;
+                color: #9B9B9B;
+                font-size: 18px;
+                font-weight: 500;
+                background: #D8D8D8;
+                margin: 32px 0 0;
                 &:hover{
                     background: #68DE5A;
+                    color: #FFF;
+
                 }
             }
         }
