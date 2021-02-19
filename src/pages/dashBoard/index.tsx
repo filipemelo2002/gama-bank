d6683b1154835f836749ddc45457f31ee36a0df4
Adriano <adrianoaluizi@gmail.com>
2021-02-19 20:35:53 -0300

    Elaborar os cards do Dashboard
    Desenvolver os cards e mensagem de boas vindas da tela

--- src/pages/dashBoard/index.tsx
+++ src/pages/dashBoard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Dashboard, Option } from './style';
+import { Dashboard, Option, Card } from './style';
 import Logo from '../../img/logoDash.png';
 import { SiCashapp } from 'react-icons/si';
 import { AiOutlineInteraction, AiOutlineSnippets } from 'react-icons/ai';
-import { FaWallet } from 'react-icons/fa';
+import { FaWallet, FaRegEye } from 'react-icons/fa';
 
 const DashBoard: React.FC = () => {
   return (
@@ -30,6 +30,13 @@ const DashBoard: React.FC = () => {
             </Option>
           </div>
         </aside>
+        <p className="bem-vindo">Olá usuário, seja bem vindo!</p>
+        <div className="exibicao">
+          <FaRegEye className="icon-eye" />
+        </div>
+        <Card className="conta" />
+        <Card className="credito" />
+        <Card className="lançamentos" />
       </Dashboard>
     </>
   );
