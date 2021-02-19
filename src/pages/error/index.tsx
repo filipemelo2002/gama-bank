20ec3407189c141bb4a592e24230c743f8a9848a
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-02-18 22:17:55 -0300

    Add Error Page styles

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiAlertTriangle, FiArrowLeft } from 'react-icons/fi';
+import Logo from "../../img/logo.png";
 
-// import { Container } from './styles';
+import { ErrorPage, Container, Content } from './style';
 
 const Error: React.FC = () => {
   return (
-    <>
-      <h1>Erro</h1>
-    </>
+    <ErrorPage>
+      <Link to="/">
+        <img className="logo-gama" src={Logo} alt="" />
+      </Link>
+      <Container>
+        <Content>
+          <FiAlertTriangle size={100} color="#8C52E5" />
+          <p>Oops, algo deu errado!</p>
+          <p>Confirme seus dados e tente novamente!</p>
+          <button>
+            Voltar
+            <FiArrowLeft size={26} />
+          </button>
+        </Content>
+      </Container>
+    </ErrorPage> 
   );
 }
 
--- src/pages/error/style.ts
+++ src/pages/error/style.ts
@@ -1,1 +1,73 @@
-import styled, {css} from "styled-components";
+import styled from 'styled-components';
+
+export const ErrorPage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  img{
+    position: absolute;
+    top: 40px;
+    left: 40px;
+  }
+`;
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 40px;
+  
+  max-width: 455px;
+  background: #fbfbfb;
+  border-radius: 8px;
+
+  svg {
+    margin-bottom: 16px;
+  }
+
+  p {
+    font-size: 24px;
+    line-height: 30px;
+    font-weight: 500;
+    text-align: center;
+  }
+
+  button{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    border: none;
+    border-radius: 50px;
+    padding: 12px 30px;
+    width: 325px;
+    text-align: left;
+    transition: 0.5s;
+    color: #FFF;
+    font-size: 16px;
+    font-weight: 500;
+    margin: 24px 0;
+    background: #68DE5A;
+    transform: 0.2s;
+
+    &:hover{
+        opacity: 0.6;
+    }
+
+    svg {
+      margin: 0;
+    }
+  }
+
+`;
