0b3aaee5b872c5c781eb454f4cda1f284bf37b3f
Adriano <adrianoaluizi@gmail.com>
2021-02-19 00:13:58 -0300

    GAMABANK-04 - Elaborar o Aside do Dashboard
    Desenvolvimento da aba do dashboard e foi instalado a lib react-icons

--- src/pages/dashBoard/index.tsx
+++ src/pages/dashBoard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Dashboard, Button } from './style';
+import Logo from '../../img/logoDash.png';
+import { SiCashapp } from 'react-icons/si';
+import { AiOutlineInteraction, AiOutlineSnippets } from 'react-icons/ai';
+import { FaWallet } from 'react-icons/fa';
 
 const DashBoard: React.FC = () => {
   return (
     <>
-      <h1>Dashboard</h1>
+      <Dashboard>
+        <aside>
+          <img src={Logo} alt="" />
+          <div className="menu">
+            <Button className="depositos">
+              <SiCashapp className="icon" />
+              <span>Depósitos</span>
+            </Button>
+            <Button className="planos">
+              <AiOutlineSnippets className="icon" />
+              <span>Planos</span>
+            </Button>
+            <Button className="pagamentos">
+              <FaWallet className="icon" />
+              <span>Pagamentos</span>
+            </Button>
+            <Button className="transações">
+              <AiOutlineInteraction className="icon" />
+              <span>Transações</span>
+            </Button>
+          </div>
+        </aside>
+      </Dashboard>
     </>
   );
 };
