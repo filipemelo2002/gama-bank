7ea99fc69395819981ab9d5fff7b26e0249cfce0
DavysonMelo <mdavyson@gmail.com>
2021-02-22 11:04:14 -0300

    Finished Loading Component

---
+++ src/components/Loading/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span {
+    margin-bottom: 10px;
+    font-weight: 300;
+  }
+`;
