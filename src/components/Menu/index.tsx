70874c1950f0de7f8993c5d09c5f8f874b1480fe
Filipe Melo <filipemelo032@gmail.com>
2021-02-24 19:03:41 -0300

    added routing links to menu options

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,21 +7,23 @@ import Pix from '../../img/pix.svg';
 const Menu: React.FC = () => {
   return (
     <Container>
-      <img src={Logo} alt="" />
+      <Link to="/dashboard/">
+        <img src={Logo} alt="" />
+      </Link>
       <div className="menu">
-        <Link to="" className="option">
+        <Link to="/dashboard/depositos" className="option">
           <img className="icon" src={Pix} />
           <span>Depósitos</span>
         </Link>
-        <Link to="" className="option">
+        <Link to="/dashboard/planos" className="option">
           <img className="icon" src={Pix} />
           <span>Planos</span>
         </Link>
-        <Link to="" className="option">
+        <Link to="/dashboard/pagamentos" className="option">
           <img className="icon" src={Pix} />
           <span>Pagamentos</span>
         </Link>
-        <Link to="" className="option">
+        <Link to="/dashboard/transacoes" className="option">
           <img className="icon" src={Pix} />
           <span>Transações</span>
         </Link>
--- src/pages/dashBoard/index.tsx
+++ src/pages/dashBoard/index.tsx
@@ -1,53 +1,39 @@
 import React from 'react';
-import { Card, CardTitle, CardContent } from './style';
 
 import Menu from '../../components/Menu';
-import { FaRegEye } from 'react-icons/fa';
-import { HiOutlineCurrencyDollar } from 'react-icons/hi';
-import { BsCreditCard } from 'react-icons/bs';
+import { Content } from './style';
+
+import { Switch, Route } from 'react-router-dom';
 
 const DashBoard: React.FC = () => {
   return (
     <>
       <Menu />
-      <p className="bem-vindo">Olá usuário, seja bem vindo!</p>
-      <div className="exibicao">
-        <FaRegEye className="icon-eye" />
-      </div>
-      <Card className="conta">
-        <CardTitle>
-          <HiOutlineCurrencyDollar /> <p>Conta</p>
-        </CardTitle>
-        <CardContent>
-          Saldo Disponivel
-          <div className="saldo">R$:&nbsp;950.451,10</div>
-          <div className="titulo">Transações</div>
-          <div className="transacoes">R$:&nbsp;120,89</div>
-        </CardContent>
-      </Card>
-      <Card className="credito">
-        <CardTitle>
-          <BsCreditCard /> <p>Conta&nbsp;Crédito</p>
-        </CardTitle>
-        <CardContent>
-          Fatura Atual
-          <div className="saldo saldo-credito">R$:&nbsp;400,00</div>
-          <div className="titulo">Limite&nbsp;Disponivel</div>
-          <div className="transacoes">R$:&nbsp;948.120,88</div>
-        </CardContent>
-      </Card>
-      <Card className="lançamentos">
-        <CardTitle>
-          <HiOutlineCurrencyDollar /> <p>Últimos Lançamentos</p>
-        </CardTitle>
-        <CardContent>
-          <BsCreditCard className="icon-compras" />
-          <div className="compras">Compra no débito</div>
-          <div className="compras data-compras">Dia 24 de Jan</div>
-          <div className="compras tipo-compra">Gama Academy</div>
-          <div className="compras valor-compra">R$:&nbsp;948.120,88</div>
-        </CardContent>
-      </Card>
+      <Content>
+        <Switch>
+          <Route exact path="/dashboard/" component={() => <h1>Home</h1>} />
+          <Route
+            exact
+            path="/dashboard/depositos"
+            component={() => <h1>Depósitos</h1>}
+          />
+          <Route
+            exact
+            path="/dashboard/planos"
+            component={() => <h1>Planos</h1>}
+          />
+          <Route
+            exact
+            path="/dashboard/pagamentos"
+            component={() => <h1>Pagamentos</h1>}
+          />
+          <Route
+            exact
+            path="/dashboard/transacoes"
+            component={() => <h1>Transações</h1>}
+          />
+        </Switch>
+      </Content>
     </>
   );
 };
