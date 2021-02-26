0ae56f51667cf60cfa6ec64324e2cadaa2da72b1
DavysonMelo <mdavyson@gmail.com>
2021-02-25 21:23:48 -0300

    PlanSelect component finished

---
+++ src/components/PlanSelect/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Select = styled.select`
+  height: 53px;
+  width: 40%;
+  border-radius: 8px;
+  border: 1px solid #9b9b9b9b;
+  color: #9b9b9b9b;
+  padding-left: 5px;
+`;
