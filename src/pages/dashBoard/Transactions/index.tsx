99775c8222493100b6130d08ba6bffeb3fddaaef
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-02-28 11:34:25 -0300

    Add transactions functions

--- src/pages/dashBoard/Transactions/index.tsx
+++ src/pages/dashBoard/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FormEvent, useCallback, useMemo, useState } from 'react';
 import {
   Row,
   Container,
@@ -12,11 +12,95 @@ import { AiOutlineHome } from 'react-icons/ai';
 import { BiDollarCircle } from 'react-icons/bi';
 import { IoSearchCircleOutline } from 'react-icons/io5';
 import TransactionItem from '../../../components/TransactionItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Logout from '../../../components/LogoutButton';
+import * as Creators from '../../../redux/action/dashboard';
+import { showError } from '../../../services/ShowToast';
+import { getMonthName } from '../../../utils/getDate';
+import { toMoney } from '../../../utils/toMoney';
+import { BsCreditCard } from 'react-icons/bs';
+
+interface ILancamentos extends ILancamentosData {
+  title: string;
+}
 
 const Transactions: React.FC = () => {
-  const { nome } = useSelector((state: State) => state.auth.usuario);
+  const dispatch = useDispatch();
+  const [list, setList] = useState<ILancamentos[]>([]);
+  const [initialDate, setInitialDate] = useState('');
+  const [finalDate, setFinalDate] = useState('');
+  const { nome, login } = useSelector((state: State) => state.auth.usuario);
+  const { loading, error, ...account } = useSelector((state: State) => {
+    return state.dashboard;
+  });
+
+  const loadTransactions = useCallback(() => {
+    const lancamentoDebito = account.contaBanco.lancamentos.map(lancamento => {
+      let title;
+
+      if (lancamento.tipo === 'D') {
+        title = 'Compra no débito';
+      } else {
+        title = 'Depósito';
+      }
+
+      return { ...lancamento, title };
+    });
+
+    const lancamentoCredito = account.contaCredito.lancamentos.map(
+      lancamento => {
+        let title;
+
+        if (lancamento.tipo === 'D') {
+          title = 'Compra no crédito';
+        } else {
+          title = 'Transferência';
+        }
+
+        return { ...lancamento, title };
+      },
+    );
+
+    const lancamentoArray = [...lancamentoDebito, ...lancamentoCredito];
+
+    setList(
+      lancamentoArray.sort(function (a, b) {
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return Number(new Date(b.data)) - Number(new Date(a.data));
+      }),
+    );
+  }, [account.contaBanco.lancamentos, account.contaCredito.lancamentos]);
+
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (initialDate === '' || finalDate === '') {
+        showError('Preencha corretamente as datas.');
+        return;
+      }
+
+      const formattedInitialDate = new Date(initialDate)
+        .toISOString()
+        .split('T')[0];
+      const formattedFinalDate = new Date(finalDate)
+        .toISOString()
+        .split('T')[0];
+
+      dispatch(
+        Creators.loadData({
+          login,
+          inicio: formattedInitialDate,
+          fim: formattedFinalDate,
+        }),
+      );
+
+      loadTransactions();
+    },
+    [initialDate, finalDate],
+  );
+
   return (
     <Container>
       <header>
@@ -32,11 +116,13 @@ const Transactions: React.FC = () => {
           <h1>Consultar transações</h1>
         </CardHeader>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <Row>
             <input
               type="text"
               placeholder="Data de início"
+              value={initialDate}
+              onChange={e => setInitialDate(e.target.value)}
               onFocus={e => (e.target.type = 'date')}
               onBlur={e => e.target.value === '' && (e.target.type = 'text')}
             />
@@ -44,6 +130,8 @@ const Transactions: React.FC = () => {
             <input
               type="text"
               placeholder="Data de fim"
+              value={finalDate}
+              onChange={e => setFinalDate(e.target.value)}
               onFocus={e => (e.target.type = 'date')}
               onBlur={e => e.target.value === '' && (e.target.type = 'text')}
             />
@@ -51,19 +139,31 @@ const Transactions: React.FC = () => {
           </Row>
         </form>
       </FirstCard>
-      <SecondCard>
-        <CardHeader>
-          <BiDollarCircle size={38} color="#9B9B9B" />
-          <h1>Lançamentos</h1>
-        </CardHeader>
-        <div>
-          <TransactionItem />
-          <TransactionItem />
-          <TransactionItem />
-          <TransactionItem />
-          <TransactionItem />
-        </div>
-      </SecondCard>
+      {list.length ? (
+        <SecondCard>
+          <CardHeader>
+            <BiDollarCircle size={38} color="#9B9B9B" />
+            <h1>Lançamentos</h1>
+          </CardHeader>
+          <div>
+            {list.map(lancamento => {
+              return (
+                <TransactionItem
+                  key={lancamento.id}
+                  id={lancamento.id}
+                  title={lancamento.title}
+                  descricao={lancamento.descricao}
+                  conta={lancamento.conta}
+                  data={lancamento.data}
+                  planoConta={lancamento.planoConta}
+                  tipo={lancamento.tipo}
+                  valor={lancamento.valor}
+                />
+              );
+            })}
+          </div>
+        </SecondCard>
+      ) : null}
     </Container>
   );
 };
